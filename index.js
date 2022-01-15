843c6b4a586afcc68de10e87e61d902bd848072e
fatihyanik <69148086+fatihyanik@users.noreply.github.com>
2022-01-15 18:27:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const {format} = require('date-fns');
-const {v4 : uuid} = require('uuid');
+const logEvents = require('./logEvents');
 
+const EventEmitter = require('events');
 
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+class MyEmitter extends EventEmitter {};
 
-console.log(uuid())
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(()=>{
+  //Emit event
+  myEmitter.emit("log","Log event emitted!');
 
