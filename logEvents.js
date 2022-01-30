49f0c0503363b1e32a714d9aea38655e7431e26e
fatihyanik <69148086+fatihyanik@users.noreply.github.com>
2022-01-30 18:25:32 +0100

    Update logEvents.js

--- logEvents.js
+++ logEvents.js
@@ -9,9 +9,12 @@ const logEvents = async (message) => {
   const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
   const logItem = `${dateTime}\t${uuid()}\t${message}`;
   try{
+    if(!fs.existsSync(path.join(__dirname, 'logs'))){
+      await fsPromises.mkdir(path.join(__dirname, 'logs'))
+    }
     await fsPromises.appendFile(path.join(__dirname, "logs", "eventlog.txt"), logItem);
   }catch(err){
-    console.err(err);
+    console.log(err);
   }
   }
 
